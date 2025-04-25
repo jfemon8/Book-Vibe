f37063a39992546b88003a981868c70bb667be67
jfemon8 <jfemon8@gmail.com>
2025-04-25 19:42:39 +0600

    Added Banner

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import Banner from "../../components/Banner/Banner";
 
 const Home = () => {
-    return (
-        <div className='mx-10 lg:mx-20'>
-            <h1>This is home page.</h1>
-        </div>
-    );
+  return (
+    <div className="mx-10 lg:mx-20">
+      <Banner></Banner>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import bookImage from "../../assets/images/books.jpg";
+import { Link } from "react-router";
+
+const Banner = () => {
+  return (
+    <div className="my-6 lg:my-12 bg-[#131313]/5 rounded-3xl lg:px-32 lg:py-20 px-16 py-10">
+      <div className="flex flex-col lg:flex-row justify-between lg:items-center gap-10">
+        <div>
+          <h1 className="playfair-display font-bold text-5xl text-[#131313] lg:leading-20">
+            Books to freshen up your bookshelf
+          </h1>
+          <Link to={"listedBook"}>
+            <a className="btn bg-[#23BE0A] border-[#23BE0A] text-white hover:bg-white hover:text-[#23BE0A] mt-5 lg:mt-10 work-sans">
+              View The List
+            </a>
+          </Link>
+        </div>
+        <div>
+          <img src={bookImage} alt="Book" className="rounded-2xl" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
